7212b61d0890c97a21b68899fc2af2593b01ddcc
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-02-16 09:40:07 +1100

    WIP: Dynamic Obstacles, Win/Lose Cons, and more
    Dynamic Obstacles, Win and Lose Conditions, Fixing agent navigation and more.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,6 +38,7 @@ public class PlayerController : MonoBehaviour
        if(count >=12) 
        {
             winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
        }
    }
 
@@ -57,5 +58,15 @@ public class PlayerController : MonoBehaviour
 
            SetCountText();
        }
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
    }
 }
