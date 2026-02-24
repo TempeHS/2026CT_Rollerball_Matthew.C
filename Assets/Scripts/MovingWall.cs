79708e66adb0d9b2381fe0dfe3721ed0ddb9b00e
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-02-25 10:34:02 +1100

    branch: Moving Wall

---
+++ Assets/Scripts/MovingWall.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovingWall : MonoBehaviour
+{
+ [SerializeField] private Vector3 moveOffset = new Vector3(0f, 3f, 0f);
+ [SerializeField] private float speed = 1.5f;
+
+     private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        float t = Mathf.PingPong(Time.time * speed, 1f);
+        transform.position = Vector3.Lerp(startPos, startPos + moveOffset, t);
+    }
+}
