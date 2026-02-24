c1bdc3b06afc99d6195c3d2f477ad8484ec60d67
Matthew C <matthew.chan654@education.nsw.gov.au>
2026-02-25 09:44:17 +1100

    fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -60,15 +60,6 @@ public class PlayerController : MonoBehaviour
        }
    }
 
-    void SpeedBoostTrigger(Collider other) 
-    {
-        if (other.gameObject.CompareTag("SpeedBoost"))
-        {
-            other.gameObject.SetActive(false);
-            public float speed = 12.5;
-        }
-    }
-
    private void OnCollisionEnter(Collision collision)
    {
         if (collision.gameObject.CompareTag("Enemy"))
